d209acad8aa2e09f62ec4b50f4474a0560991d88
Raffaello <105170007+RaffaelloC@users.noreply.github.com>
2022-05-29 13:24:35 +0000

    Changes in functions in vector.c
    I changed both push and insert functions to void. I forgot vectors are pointers and since they are, I can change the values without having to return another pointer.
    The code is more concise.
    With time I will add more functions (so it can change values in specific parts, and perhaps if I git gud I will try to support multidimensional vectors) and I will also add more comments.
    I want to thank my friend whose github account I do not recall.

--- vector.c
+++ vector.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
 typedef int* vector;
 
 vector create(int temp[], int length){
@@ -13,49 +12,42 @@ vector create(int temp[], int length){
     return array;
 }
 
-vector push(vector array,int num){
+void push(vector array,int num){
     int i = 0;
     while((array[i+1] != 0)){
         i++;
     }
     array[i] = num;
     array[i+1] = i+1;
-    return array;
 }
 
-vector delete(vector array, int n){
+void delete(vector array, int n){
     int j = 0;
-    vector temp = malloc(sizeof(array));
-    for(int i=0; i<n; i++){
-        temp[i] = array[i];
-    }
-
     while((array[j+1] != 0)){
         j++;
     }
     array[j]--;
     for(int i=(n+1); i<=j; i++){
-        temp[i-1] = array[i];
+        array[i-1] = array[i];
     }
-    return temp;
+    array[j] = 0;
 }
 
 int main(){
     int array[] = {1,2,3,4,5,6,7,8,9,10};
-    char fra = 'a';
     size_t length = sizeof(array)/sizeof(array[0]);
     vector vec = create(array, length);
     for(int i=0; i<12; i++){
         printf("%d \t", vec[i]);
     }
     printf("\n");
-    vec = push(vec, 11);
-    vec = push(vec, 30);
+    push(vec, 11);
+    push(vec, 30);
     for(int i=0; i<13; i++){
         printf("%d \t", vec[i]);
     }
      printf("\n");
-    vec = delete(vec, 4);
+    delete(vec, 4);
     for(int i=0; i<13; i++){
         printf("%d \t", vec[i]);
     }
